7227ef39a0443622f8263486eae0e8c9b921d8a3
Ryan Lee <therealsassyfatcat@outlook.com>
2020-07-16 14:39:50 -0500

    saving

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import ImageGrid from './ImageGrid'
 import "./App.css";
 import styled from 'styled-components';
 
+const AppH1 = styled.h1`
+  color: white;
+`;
+
+const AppP = styled.p`
+  color:white;
+`;
+
 const AppDiv = styled.div`
 background: rgba(23, 23, 23, .7);
 padding: 3%;
@@ -15,13 +23,13 @@ const SearchBar = styled.div`
   display: flex;
   justify-content: center;
   align-items:center;
-  /* ${props => (props.searched === true) ? 'padding: 2%' : null} */
 `;
 
 const TextInput = styled.input`
-  background: white;
-  border: 2px solid black;
+  background: rgba(23, 23, 23, .7);
+  border: 2px solid white;
   border-radius: 5px;
+  color: white;
 `;
 
 const IconSpan = styled.span`
@@ -29,7 +37,8 @@ const IconSpan = styled.span`
     cursor: pointer;
   }
   i {
-    font-size: 1.5rem;
+    font-size: 1.8rem;
+    color: white;
   }
 `;
 
@@ -39,10 +48,14 @@ const [searchRequest, setSearchRequest] = useState("");
 const [textSize, setTextSize] = useState(1);
 const [appWidth, setAppWidth] = useState(false)
 
+useEffect(() => {
+searchRequest === "" ? setAppWidth(false) : setAppWidth(true)
+}, [searchRequest])
+
   return (
     <AppDiv searched={appWidth} className="App">
-      {searchRequest === "" && <h1>Search for NASA Images</h1>}
-      {searchRequest === "" && <p>Background image compliments of NASA Photo of The Day</p>}
+      {searchRequest === "" && <AppH1>Search for NASA Images</AppH1>}
+      {searchRequest === "" && <AppP>Background image compliments of NASA Photo of The Day</AppP>}
       <SearchBar searched={appWidth}>
         <label><TextInput type="text" name="search" size={textSize} onChange={event => setSearchTerm(event.target.value)} onMouseOver={() => setTextSize(20)} onMouseLeave={() => setTextSize(searchTerm.length)}></TextInput>
             </label> 
